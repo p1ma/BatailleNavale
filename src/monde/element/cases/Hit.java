9577e3500b98486ad77f0e33955195692230ba45
p1ma <pierre.junges@orange.fr>
2017-03-02 21:59:20 +0100

    ajouts de sub classes

---
+++ src/monde/element/cases/Hit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package monde.element.cases;
+
+import java.awt.Point;
+
+/**
+ * @author JUNGES Pierre-Marie - M1 Informatique 2016/2017
+ *
+ * Mar 2, 2017
+ */
+public class Hit extends Case{
+
+	public Hit(int nbCase, int lrgr, String path, Point pos) {
+		super(nbCase, lrgr, path, pos);
+		// TODO Auto-generated constructor stub
+	}
+
+}
