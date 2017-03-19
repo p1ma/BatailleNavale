7135a82838cf0de353320a6bb6a60af42ac3c341
bellec1u <leopold.bellec@gmail.com>
2017-03-19 12:13:23 +0100

    [ADD] ecran de separation

---
+++ src/graphics/SeparationScreen.java
@@ -0,0 +1,24 @@
+package graphics;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import game.Configuration;
+import game.Game;
+import graphics.listener.BoardController;
+
+public class SeparationScreen extends JPanel {
+
+	public SeparationScreen(final int unit) {
+		// SIZE
+		this.setPreferredSize(new Dimension(unit, unit * Configuration.HEIGHT));
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+	
+}
